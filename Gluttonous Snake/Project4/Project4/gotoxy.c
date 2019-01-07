8916fa0fc606edec93f5559da3b9707fba498f5f
IEXPECTATIOM <1120644453@qq.com>
2019-01-07 20:15:24 +0800

    一个简单的练习

---
+++ Gluttonous Snake/Project4/Project4/gotoxy.c
@@ -0,0 +1,10 @@
+﻿#include <Windows.h>
+#include "info.h"
+
+void gotoxy(int x, int y)
+{
+	COORD ipos;
+	ipos.X = x;
+	ipos.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), ipos);
+}
